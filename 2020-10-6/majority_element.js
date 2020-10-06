17bb6260cd444b8122d93fe94ef3b8c186f09980
camden <3293676794@qq.com>
2020-10-06 15:51:40 +0800

    分治算法

--- 2020-10-6/majority_element.js
+++ 2020-10-6/majority_element.js
@@ -1,11 +1,37 @@
 /**
- * @param {number} N
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-let divisorGame = function(N) {
-  return N % 2 === 0;
+let majorityElement = function(nums) {
+
+  let countInRange = (countNums, num, lo, hi) => {
+    let count = 0;
+    for (let i = lo; i <= hi; i++) {
+      if (countNums[i] === num) count++;
+    }
+    return count;
+  }
+
+  let majorityElementRec = (recNums, lo, hi) => {
+    if (lo === hi) return recNums[lo];
+
+    let mid = (hi - lo) / 2;
+    mid = Math.floor( mid ) + lo;
+    let left = majorityElementRec(recNums, lo, mid);
+    let right = majorityElementRec(recNums, mid + 1, hi);
+
+    if (left === right) return left;
+
+    let leftCount = countInRange(recNums, left, lo, hi);
+    let rightCount = countInRange(recNums, right, lo, hi);
+
+    return leftCount > rightCount ? left : right;
+  }
+
+  return majorityElementRec(nums, 0, nums.length-1);
+
 };
 
 document.getElementsByClassName("content")[0]
-  .innerHTML = minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
+  .innerHTML = majorityElement([1, 2, 2, 1, 1, 4])
   .toString();
