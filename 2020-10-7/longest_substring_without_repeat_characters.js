b4b421639f1ffb66c077955710c40a579db9d965
camden <3293676794@qq.com>
2020-10-10 19:16:35 +0800

    无重复字符的最长子串_3

--- 2020-10-7/longest_substring_without_repeat_characters.js
+++ 2020-10-7/longest_substring_without_repeat_characters.js
@@ -1,55 +1,26 @@
-function ListNode(val) {
-  this.val = val;
-  this.next = null;
-}
 /**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
+ * @param {string} s
+ * @return {number}
  */
-let addTwoNumbers = function(l1, l2) {
-  let p = 0, q = 0;
-  let temp1 = l1;
-  let temp2 = l2;
-  let res = new ListNode(0);
-  let temp3 = res;
-
-  while (temp1 !== null || temp2 !== null) {
-    if (temp1 === null) {
-      q = 0 + temp2.val + p;
-    }else if (temp2 === null) {
-      q = temp1.val + 0 + p;
-    }else {
-      q = temp1.val + temp2.val + p;
+//应该用new Set（）做而不是对象
+let lengthOfLongestSubstring = function(s) {
+  let sArr = s.split("");
+  let r = s.length >= 1 ? 1 : 0;
+  let map = {};
+  for (let i = 0; i < sArr.length; i++) {
+    for (let j = i; j < sArr.length; j++) {
+      if (map.hasOwnProperty(sArr[j])) {
+        r = Object.getOwnPropertyNames(map).length > r ? Object.getOwnPropertyNames(map).length : r;
+        map = {};
+        break;
+      }else {
+        map[sArr[j]] = sArr[j];
+      }
     }
-
-    if (q >= 10) {
-      p = Math.floor(q/10);
-      q = q % 10;
-      temp3.next = new ListNode(q);
-    }else {
-      p = Math.floor(q/10);
-      temp3.next = new ListNode(q);
-    }
-    if (temp1 !== null) temp1 = temp1.next;
-    if (temp2 !== null) temp2 = temp2.next;
-    temp3 = temp3.next;
   }
-  if (p !== 0) temp3.next = new ListNode(p);
-  return res.next;
-
+  return r;
 };
 
-let l1 = new ListNode(2);
-l1.next = new ListNode(4);
-l1.next.next = new ListNode(3);
-
-let l2 = new ListNode(5);
-l2.next = new ListNode(6);
-l2.next.next = new ListNode(4);
-
-console.log(addTwoNumbers(l1,l2))
-
 document.getElementsByClassName("content")[0]
-  .innerHTML = addTwoNumbers(l1,l2)
+  .innerHTML = lengthOfLongestSubstring("pwwkew")
   .toString();
