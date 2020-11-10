3333befc4c8574c345373991f56509300497688d
camden <3293676794@qq.com>
2020-11-10 22:29:19 +0800

    13-三数之和-有bug

---
+++ 2020-11-10/three_sum.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+let threeSum = function(nums) {
+  let n = nums.length;
+  function compare(a,b){
+    return a - b;
+  }
+  nums.sort(compare);
+  let res =[[]];
+
+  for (let i = 0; i < n; i++) {
+    let k = n - 1;
+    if (i === 0 || nums[i] !== nums[i - 1]) {
+      for (let j = i + 1; j < n; j++) {
+        if ((j === i + 1 || nums[j] !== nums[j - 1]) && j < k) {
+          while (nums[i] + nums[j] + nums[k] > 0) k--;
+          if (j >= k) break;
+          if (nums[i] + nums[j] + nums[k] === 0) res.push([nums[i], nums[j], nums[k]]);
+        }
+      }
+    }
+  }
+  if (res.length > 1) {
+    res.shift();
+  } else {
+    res = [];
+  }
+  return res
+};
+
+
+document.getElementsByClassName("content")[0]
+  .innerHTML = threeSum([-2,0,1,1,2])
+  .toString();
