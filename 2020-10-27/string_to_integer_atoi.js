7df1508b96cd186ff80a05f3402ddaff5605f198
sublinenhyLabPC <3293676794@qq.com>
2020-11-25 09:43:01 +0800

    pc同步20201125“

--- 2020-10-27/string_to_integer_atoi.js
+++ 2020-10-27/string_to_integer_atoi.js
@@ -1,46 +1,12 @@
 /**
  * @param {string} s
- * @param {number} numRows
- * @return {string}
+ * @return {number}
  */
-let convert = function(s, numRows) {
-  let isFullStack = true;
-  let sArr = s.split("");
-  let stack = [];
-  while (sArr.length !== 0) {
-    if (isFullStack) {
-      let a = [];
-      for (let i = 0; i < numRows; i++) {
-        a.push(sArr.shift());
-      }
-      stack.push(a);
-      isFullStack = false;
-    } else {
-      let a = [];
-      for (let i = 0; i < numRows; i++) {
-        if (i === 0 || i === numRows-1) {
-          a.push(null)
-        } else {
-          a.push(sArr.shift());
-        }
-      }
-      stack.push(a.reverse());
-      isFullStack = true;
-    }
-  }
+let myAtoi = function(s) {
 
-  let res = "";
-  for (let k = 0; k < numRows; k++) {
-    for (let j = 0; j < stack.length; j++) {
-      if (stack[j][k] !== null && stack[j][k] !== undefined) {
-        let r = stack[j][k];
-        res += r;
-      }
-    }
-  }
-  return res
+
+
 };
-
 
 document.getElementsByClassName("content")[0]
   .innerHTML = convert("PAYPALISHIRING", 3)
